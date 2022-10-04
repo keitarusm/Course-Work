a3946b04077ba269970a8635a7806e32f07f27d5
SSVT <keitarusm@gmail.com>
2022-10-04 19:15:15 -0400

    improved fizzBuzz by actually doing the exercise.

--- js-basics/index.js
+++ js-basics/index.js
@@ -2,10 +2,12 @@
 // Divisible by 5 => Buzz
 // Divisible by 3 and 5 => FizzBuzz
 // Not divisible by 3 or 5 => input
+// Not a number => 'Not a number'
 
 console.log(fizzBuzz(15));
 
 function fizzBuzz(n) {
+    if (typeof n !== 'number') return "Not a number";
     let output = "";
     if (n % 3 === 0) output += "Fizz";
     if (n % 5 === 0) output += "Buzz";
