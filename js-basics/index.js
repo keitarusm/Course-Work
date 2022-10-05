e4e1b98dd02aea967a224cc7f9f3eeba1e1c913a
SSVT <keitarusm@gmail.com>
2022-10-04 20:24:30 -0400

    showNumbers exercise.

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,15 +1,9 @@
-// Speed Limit = 70
-// 5 -> 1 point
-// Math.floor(1.3)
-// 12 points -> suspended
-console.log(checkSpeed(73));
+showNumbers(10);
 
-function checkSpeed(speed) {
-    const speedLimit = 70;
-    if (speed < speedLimit + 5) return "Ok"
-    else {
-        speed -= speedLimit;
-        let points = Math.floor(speed / 5)
-        return points <= 12 ? ["Ok", points] : ["Suspended", points];
+function showNumbers(number) {
+    if (typeof number !== 'number') return false;
+
+    for (let i = 0; i <= number; i++) {
+        console.log(i, (i%2===0?'EVEN':'ODD'));
     }
 }
