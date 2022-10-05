4431a73b0a69c42f833fa2ef2ff21f760c6bab2b
SSVT <keitarusm@gmail.com>
2022-10-04 21:45:48 -0400

    showStars exercise. I like mine better.

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,31 +1,9 @@
-const marks = [80, 80, 50];
+showStars(3);
 
-console.log(calculateGrade(marks));
-
-function calculateGrade(marks) {
-    let grade = 0;
-
-    for (let mark of marks)
-        grade += mark;
-    grade /= marks.length;
-
-    switch (Math.floor(grade/10)) {
-        case 10:
-            return 'A';
-            break;
-        case 9:
-            return 'A';
-            break;
-        case 8:
-            return 'B';
-            break;
-        case 7:
-            return 'C';
-            break;
-        case 6:
-            return 'D';
-            break;
-        default:
-            return 'F';
+function showStars(rows) {
+    let stars = '';
+    for (let i = 0; i < rows; i++) {
+        stars += '*';
+        console.log(stars);
     }
 }
