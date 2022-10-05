55d0118a87481df2e24c5813ba5f61d52ed966a9
SSVT <keitarusm@gmail.com>
2022-10-04 21:17:22 -0400

    showProperties exercise.

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,19 +1,16 @@
-// Falsy
-// undefined
-// null
-// ''
-// false
-// 0
-// NaN
+const movie = {
+    title: 'a',
+    releaseYear: 2018,
+    rating: 4.5,
+    director: 'b',
+    runningTime: 122,
+    studio: 'c'
+};
 
-const array = [0, null, undefined, '', 2, 3, 1];
+showProperties(movie);
 
-console.log(countTruthy(array));
-
-function countTruthy(array) {
-    let count = 0;
-    for (let item of array)
-        if (item) 
-            count++;
-    return count;
+function showProperties(obj) {
+    for (let key in obj)
+        if (typeof obj[key] === 'string')
+            console.log(key, obj[key]);
 }
