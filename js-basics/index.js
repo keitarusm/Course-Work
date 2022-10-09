06ed13b6ed9285ede3c58fd712c57236d6fcbf5a
SSVT <keitarusm@gmail.com>
2022-10-09 17:40:26 -0400

    constructor practice exercise.

--- js-basics/index.js
+++ js-basics/index.js
@@ -17,3 +17,16 @@ const blogPost = {
     ],
     isLive: true
 }
+
+function BlogPost(title, body, author) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+}
+
+let blogPost2 = new BlogPost('a', 'b', 'c');
+
+console.log(blogPost2);
