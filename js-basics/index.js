d33a0d05452f6c02336add90ac573f6ffd48adaa
SSVT <keitarusm@gmail.com>
2022-10-09 17:34:54 -0400

    object literal practice.

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,49 +1,19 @@
-// street
-// city
-// zipCode
-// showAddress(address)
+// title
+// body 
+// author
+// views
+// comments
+//   (author, body)
+// isLive
 
-const address = {
-    street: '5228 VT-14',
-    city: 'Sharon VT',
-    zipCode: '05065'
+const blogPost = {
+    title: 'Article Title',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, non!',
+    author: 'Joe Bishop',
+    views: 214,
+    comments: [
+        { author: 'Sam', body: 'Cool beans.'},
+        { author: 'Paul', body: '10 out of 10'},
+    ],
+    isLive: true
 }
-
-function showAddress(address) {
-    console.log(address['street'] + '\n' + address['city'] + ', ' + address['zipCode']);
-}
-
-function createAddress(street, city, zipCode) {
-    return {
-        street, 
-        city, 
-        zipCode
-    }
-}
-
-class Address {
-    constructor(street, city, zipCode) {
-        this.street = street;
-        this.city = city;
-        this.zipCode = zipCode;
-    }
-    
-    areEqual(address2) {
-        return this.street === address2.street && 
-            this.city === address2.city && 
-            this.zipCode === address2.zipCode;
-    };
-    
-    areSame(address2) {
-        return this === address2;
-    };
-}
-
-showAddress(address);
-const address2 = createAddress('1331 Braintree Hill Rd', 'Braintree VT', '05060');
-showAddress(address2);
-const address3 = new Address('115 Etna Rd', 'Leabnon NH', '03766');
-showAddress(address3);
-const address4 = new Address('115 Etna Rd', 'Leabnon NH', '03766');
-console.log(address3.areEqual(address4));
-console.log(address3.areSame(address4));
