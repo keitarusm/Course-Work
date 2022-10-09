16b1afb3b4d251e66cf06f7eafdd978d8665a30f
SSVT <keitarusm@gmail.com>
2022-10-09 17:07:11 -0400

    added factory + constructor functions for address

--- js-basics/index.js
+++ js-basics/index.js
@@ -9,8 +9,26 @@ let address = {
     zipCode: '05065'
 }
 
-showAddress(address);
-
 function showAddress(address) {
     console.log(address['street'] + '\n' + address['city'] + ', ' + address['zipCode']);
 }
+
+function createAddress(street, city, zipCode) {
+    return {
+        street, 
+        city, 
+        zipCode
+    }
+}
+
+function Address(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+}
+
+showAddress(address);
+const address2 = createAddress('1331 Braintree Hill Rd', 'Braintree VT', '05060');
+showAddress(address2);
+const address3 = new Address('115 Etna Rd', 'Leabnon NH', '03766');
+showAddress(address3);
